c50493afe5cd9a59389bc65143a89f910c50ede2
Nicole Arwood <svilowski@gmail.com>
2023-06-05 17:03:12 -0400

    First attempt

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
+
+    const { dispatch } = useContext(AppContext);
     const { budget } = useContext(AppContext);
+    //const [budget, setBudget] = useState('');
+
+    const addBudget = (props) => {
+        dispatch({
+            type: 'ADD_BUDGET',
+            payload: props,
+        });
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+                <input
+                required='required'
+                type='number'
+                step='10'
+                max='20000'
+                id='budget'
+                value={budget}
+                style={{ marginLeft: '2rem' , size: 10}}
+                onChange={(event) => addBudget(event.target.value)}>
+                </input>    
+            </span>
         </div>
     );
 };
